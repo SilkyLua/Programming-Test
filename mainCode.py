f49abd7424a1461be7d49404c0cabdf5f8b7dba4
Dini200 <154989617+Dini200@users.noreply.github.com>
2026-04-06 12:52:33 -0400

    I added a line of code

--- mainCode.py
+++ mainCode.py
@@ -3,3 +3,7 @@ name = input("What is your name? ")
 print(f'Hello {name}, welcome to the matrix.')
 age = input("age: ")
 print(age)
+
+"Elmedin Behluli added this line to the code"
+color = input("Favorite color? ")
+print(f"{color} is a nice choice!")
