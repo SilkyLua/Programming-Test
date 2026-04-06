218d6d25a3c2b838c7e065bda04601fda7adb575
Zaboomafoo99 <zabi.hasanzadeh@mohawkcollege.ca>
2026-04-06 13:14:20 -0400

    Update mainCode.py

--- mainCode.py
+++ mainCode.py
@@ -5,3 +5,13 @@ print(f'Hello {name}, welcome to the matrix.')
 "Elmedin Behluli added this line to the code"
 color = input("Favorite color? ")
 print(f"{color} is a nice choice!")
+
+#Zabi added this beautiful code
+age = input("What is your age? ")
+while True:
+  if age.isnumeric():
+    age = int(age)
+    break
+  else:
+    age = input("What is your age? ")
+print(f"Your age is, {age}")
