ffbd39027a450a981aa00168dbf354a687d151cc
SilkyLua <107939506+SilkyLua@users.noreply.github.com>
2026-04-06 13:11:15 -0400

    Remove age input

--- mainCode.py
+++ mainCode.py
@@ -1,8 +1,6 @@
 print("This is a public repository!")
 name = input("What is your name? ")
 print(f'Hello {name}, welcome to the matrix.')
-age = input("age: ")
-print(age)
 
 "Elmedin Behluli added this line to the code"
 color = input("Favorite color? ")
