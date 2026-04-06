3a779fb49127c14a9b754e637fdb614efdcc3ea5
Zaboomafoo99 <zabi.hasanzadeh@mohawkcollege.ca>
2026-04-06 13:04:41 -0400

    Whileloop age mainCode.py
    I added a while loop that validates age input and prints it

--- mainCode.py
+++ mainCode.py
@@ -1,3 +1,13 @@
 print("This is a public repository!")
 name = input("What is your name? ")
 print(f'Hello {name}, welcome to the matrix.')
+age = input("What is your age? ")
+while True:
+  if age.isnumeric():
+    age = int(age)
+    break
+  else:
+    age = input("What is your age? ")
+print(f"Your age is, {age}")
+
+  
